64280b4d90a1c580f2770e069aea3c5e88c60dd9
fantasticaestudio <sergio.linero@fantasticaestudio.com>
2025-02-08 19:32:49 +0100

    Project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import antfu from '@antfu/eslint-config'
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(await antfu({
+  stylistic: true,
+  rules: {
+    'style/comma-dangle': ['error', 'never']
+  }
+}))
